0f073b0650436a159fb34ed2bf433cdac80c9c28
Egor Logutov <39864474+LogutovEgor@users.noreply.github.com>
2018-10-16 11:10:06 +0300

    1

--- IsputCSharpWinFormsV2/ReadWriteTest.cs
+++ IsputCSharpWinFormsV2/ReadWriteTest.cs
@@ -4,6 +4,7 @@ using System.Text;
 using System.Runtime.Serialization;
 using System.IO;
 using System.Xml.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization.Formatters;
 
 namespace IsputCSharpWinFormsV2
@@ -13,42 +14,45 @@ namespace IsputCSharpWinFormsV2
         private static ReadWriteTest Instance;
         public void ReadTest(string filePath)
         {
+            ///через XML
+            /*
             // передаем в конструктор тип класса
             XmlSerializer formatter = new XmlSerializer(typeof(Test));
             //
             StreamReader streamReader = new StreamReader("myFile.tst");
             Manager.GetInstance().CurrentTest = (Test)formatter.Deserialize(streamReader);
             streamReader.Dispose();
-            streamReader.Close();
-        }
-        public void WriteTest()
-        {   
+            streamReader.Close();*/
 
-
-            // передаем в конструктор тип класса
-            XmlSerializer formatter = new XmlSerializer(typeof(Test));
-            
-            
-            /*// получаем поток, куда будем записывать сериализованный объект
-            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
+            ///через бинарный форматер
+            BinaryFormatter binaryFormatter = new BinaryFormatter();
+            using (FileStream fs = new FileStream("myFile.tst", FileMode.OpenOrCreate))
             {
-                formatter.Serialize(fs, bla1);
-
+                Manager.GetInstance().CurrentTest = (Test)binaryFormatter.Deserialize(fs);
             }
 
-            // десериализация
-            using (FileStream fs = new FileStream("persons.xml", FileMode.OpenOrCreate))
-            {
-                bla bla2 = (bla)formatter.Deserialize(fs);
+        }
+        public void WriteTest()
+        {
 
-            }*/
+            ///Через XML
+            // передаем в конструктор тип класса
+            /*
+            XmlSerializer formatter = new XmlSerializer(typeof(Test));
             StreamWriter streamWriter = new StreamWriter("myFile.tst", false);
-            
             formatter.Serialize(streamWriter, Manager.GetInstance().CurrentTest);
             streamWriter.Dispose();
             streamWriter.Close();
-
-
+            */
+            /// через бинарный форматер
+            // создаем объект BinaryFormatter
+            BinaryFormatter formatter = new BinaryFormatter();
+            // получаем поток, куда будем записывать сериализованный объект
+            using (FileStream fs = new FileStream("myFile.tst", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, Manager.GetInstance().CurrentTest);
+            }
+           
         }
         public static ReadWriteTest GetInstance()
         {
